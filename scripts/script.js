365b3de0549fc49bf0bdaee5e8610b370ae0fc80
montygal <sariahtanner@gmail.com>
2022-09-19 18:20:34 -0600

    setup homepage

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+//const currentDate = newDate();
+//const copyright = "&copy";
+//const name = "|Sariah E.|Tanner|Utah";
+//document.querySelector('#year').innerHTML = copyright.concat(currentDate.getFullYear(), name);
+//const lastUpdate = "Last Updated:";
+//document.querySelector('#last-update').textContent = lastUpdate.concat(document.lastModified);
+//alert(document.lastModified);
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = newDate();
+
+let dayName = daynames[d.getDay()];
+
+let monthName = months[d.getMonth()];
+
+let year = d.getFullYear();
+
+let fulldate = dayName + ", " + monthName + ", " + d.getDate() +", " + year;
+
+document.getElementById("currentdate").textContent = fulldate;
+
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById(
+        "currentdate"
+    ).textContent = new Date().toLocaleDateString("en-US", options);
+} catch(e) {
+    alert("Error with code or your browser doesn't support Locale");
+}
