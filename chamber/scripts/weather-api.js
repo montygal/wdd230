dc667989c0915f318d873c57f7552f6fcc128d4e
Montygal <sariahtanner@gmail.com>
2022-11-16 15:51:29 -0700

    weather api

--- chamber/scripts/weather-api.js
+++ chamber/scripts/weather-api.js
@@ -1,13 +1,16 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const temperature = document.querySelector('#temperature');
+const description = document.querySelector('#currently');
+const weathericon = document.querySelector('#weathericon');
+const caption = document.querySelector('figcaption');
+const apiURL='https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=ce3ac5b533f9c34cc23975fa9d566bf8';
 
 async function apiFetch() {
     try {
-      const response = await fetch(url);
+      const response = await fetch(apiURL);
       if (response.ok) {
         const data = await response.json();
         console.log(data); 
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
@@ -18,15 +21,14 @@ async function apiFetch() {
   
   apiFetch();
   
+  function capitalize(string){
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
 
-  function  displayResults(weatherData) {
-  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  }
 
-  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-  const desc = weatherData.weather[0].description;
-
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = desc;
-}
-  apiFetch();
+  function displayResults(data){
+    temperature.textContent = data.main.temp.toFixed(0);
+    description.textContent = capitalize(data.weather[0].description);
+    caption.textContent = data.weather[0].description;
+    weathericon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  }
